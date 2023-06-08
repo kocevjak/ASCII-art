4aabe12cd9f6e70af3709430ccff071b339721e8
jakub-star <jakubkocev@gmail.com>
2023-06-08 15:59:10 +0200

    cllas picture.h

--- picture.h
+++ picture.h
@@ -2,16 +2,20 @@
 #define PICTURE_H
 
 #include <QPicture>
+#include <QPixmap>
+#include <QString>
 
 
 class picture
 {
 private:
-
+    QString AsciiIm;    //proměná pro uložení onrazku ve formátu ASCII
 public:
     picture(QPicture im); //konstruktor
 
+    QPixmap getPixmap();    //vrátí QPiaxmap
 
+    QString getAsciiIm();   //vrátí ascii obrazek
 
     QPicture im;//proměná pro uložení obrazku
 };
