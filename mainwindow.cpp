d2473811419b4216bd4d537c76afe72bce75180f
jakub-star <jakubkocev@gmail.com>
2023-06-10 23:14:23 +0200

    fix designe

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,20 +12,21 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    QPushButton *b = new QPushButton();
+    this->setCentralWidget(this->ui->widget);
+    this->ui->widget->setLayout(this->ui->layout);
 
+    QPushButton *b = new QPushButton(this);
     b->setText("neco");
-    QPushButton *b2 = new QPushButton();
+    QPushButton *b2 = new QPushButton(this);
     b2->setText("neco 2");
-    layout->addWidget(b);
-    layout->addWidget(b2);
-
+    ui->layout->addWidget(b);
+    ui->layout->addWidget(b2);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 
 
